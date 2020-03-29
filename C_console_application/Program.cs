a0d5018f0a31d67800877e57e9b5f710ddc2567a
mai1121 <maimaiukawa@gmail.com>
2020-03-29 20:19:23 +0900

    first commit

---
+++ C_console_application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_console_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
